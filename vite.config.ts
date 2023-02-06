85faad5e124891a49e04a018b6d6e2594fc9c511
XTDragon <1686365414@qq.com>
2023-02-06 10:52:06 +0800

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
         proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
             // 正则表达式写法
             '^/api': {
-                target: 'http://127.0.0.1:9090/', // 后端服务实际地址
+                target: 'http://114.55.64.236:9090/', // 后端服务实际地址
                 changeOrigin: true, //开启代理
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
