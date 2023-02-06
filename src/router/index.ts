b919b91d1c1c25774b1d65d5b636d1ef6ae72c82
XTDragon <1686365414@qq.com>
2023-02-06 10:04:21 +0800

    Changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,9 +8,7 @@ import TDGame from "/src/views/TDGame.vue"
 // 每个路由都需要映射到一个组件。
 // 我们后面再讨论嵌套路由。
 const routes = [
-    {path: '/', component: index},
-    {path: '/test', component: test},
-    {path: '/2', component: fc},
+    {path: '/', component: TDGame},
     {path: '/main', component: TDGame},
 ]
 
