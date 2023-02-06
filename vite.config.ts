347b0ef0a22651c31148671d599f03d732d491be
XTDragon <1686365414@qq.com>
2023-02-06 13:05:35 +0800

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
     //     },
     // },
     server: {
-        port: 3000,
+        port: 9090,
         open: false, //自动打开
         base: "./ ", //生产环境路径
         proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
